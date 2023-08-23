8847a533f79feead02fa38689ecf3924c09638a4
Ralph Rosael <ralphrosael27@gmail.com>
2023-08-23 22:54:06 +0800

    feat: add AboutUs

---
+++ src/components/AboutUs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2 style={{ textAlign: 'center' }}>About Us</h2>
+      <p>
+        Welcome to our website! We are dedicated to providing you with the best tourist information and recommendations.
+        Our team of travel enthusiasts is here to help you discover amazing destinations and plan your perfect trip.
+      </p>
+      <p>
+        Whether you're an adventure seeker, a history buff, or just looking to relax on a beautiful beach, we've got you covered.
+        Explore our website to find travel guides, tips, and inspiration for your next unforgettable journey.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
