0f17c2995a511a2590e4147710a378e180d49f6b
Ralph Rosael <90777662+coder-ralph@users.noreply.github.com>
2023-09-11 16:27:57 +0800

    Update the date format

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
+  const options = { year: 'numeric', month: 'short', day: '2-digit' };
+  const formattedDate = new Date(post.updatedAt).toLocaleDateString('en-US', options);
+
   return (
     <article>
       <h2 className='title'>{post.title}</h2>
       <div className="published-time">
-        <i className='far fa-clock'></i>
-        <span>{new Date(post.updatedAt).toDateString()}</span>
+        <i className='far fa-calendar blue-icon'></i>
+        <span>{formattedDate}</span>
       </div>
 
-      <div className='content' dangerouslySetInnerHTML={{__html: post.content.html}} />
+      <div className='content' dangerouslySetInnerHTML={{ __html: post.content.html }} />
     </article>
-  )
+  );
 }
 
-export default Post
+export default Post;
