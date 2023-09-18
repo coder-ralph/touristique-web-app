105d29b1a4210cd782b1ce0c6e17568e1bbe3e34
Ralph Rosael <90777662+coder-ralph@users.noreply.github.com>
2023-09-18 22:50:01 +0800

    Refactor Pagination Components to Scroll to Top on Page Change

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-const Pagination = ({totalPage}) => {
+const Pagination = ({ totalPage }) => {
   const { search } = useLocation();
   const [pageNumbers, setPageNumbers] = useState([]);
   const page = Number(new URLSearchParams(search).get('page')) || 1;
 
+  // Scroll to the top of the page whenever the page changes
   useEffect(() => {
-    if(totalPage <= 1) return;
+    window.scrollTo(0, 0);
+  }, [page]);
 
-    if(totalPage <= 5){
+  useEffect(() => {
+    if (totalPage <= 1) return;
+
+    if (totalPage <= 5) {
       const newArr = [...Array(totalPage)].map((_, i) => i + 1);
       return setPageNumbers(newArr);
     }
@@ -17,46 +22,48 @@ const Pagination = ({totalPage}) => {
     let newArr = [];
 
     for (let index = 1; index <= 5; index++) {
-      if(page <= 2){
-        newArr.push(index)
+      if (page <= 2) {
+        newArr.push(index);
       }
 
-      if(page > 2 && page < totalPage - 2){
-        newArr.push(page + index - 3)
+      if (page > 2 && page < totalPage - 2) {
+        newArr.push(page + index - 3);
       }
-      
-      if(page >= totalPage - 2){
-        newArr.push(index - 5 + totalPage)
+
+      if (page >= totalPage - 2) {
+        newArr.push(index - 5 + totalPage);
       }
     }
 
     setPageNumbers(newArr);
-  }, [totalPage, page])
-  
+  }, [totalPage, page]);
 
-  if(totalPage <= 1) return null;
+  if (totalPage <= 1) return null;
   return (
     <div className='pagination'>
-      <Link to={`?page=${page - 1 <= 1 ? 1 : page - 1}`} className="prev-next">
+      <Link
+        to={`?page=${page - 1 <= 1 ? 1 : page - 1}`}
+        className="prev-next"
+      >
         <i className='fa-solid fa-arrow-left' /> Prev
       </Link>
 
       <ul className="numbers">
-        {
-          pageNumbers.map(num => (
-            <Link to={`?page=${num}`} key={num}>
-              <li className={page === num ? 'active' : ''}>{num}</li>
-            </Link>
-          ))
-        }
+        {pageNumbers.map(num => (
+          <Link to={`?page=${num}`} key={num}>
+            <li className={page === num ? 'active' : ''}>{num}</li>
+          </Link>
+        ))}
       </ul>
 
-      <Link to={`?page=${page + 1 >= totalPage ? totalPage : page + 1}`}
-      className="prev-next">
-        Next <i className='fa-solid fa-arrow-right' /> 
+      <Link
+        to={`?page=${page + 1 >= totalPage ? totalPage : page + 1}`}
+        className="prev-next"
+      >
+        Next <i className='fa-solid fa-arrow-right' />
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
