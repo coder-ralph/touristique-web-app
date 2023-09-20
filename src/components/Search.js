c3a57744867e50b78ddf81254678ba3fdd71c570
Ralph Rosael <90777662+coder-ralph@users.noreply.github.com>
2023-09-20 12:32:51 +0800

    Update the Search button on mobile device

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
   const navigate = useNavigate();
   const [search, setSearch] = useState('');
 
-  function handleSearch(e){
+  function handleSearch(e) {
     e.preventDefault();
-    setSearch("")
+    setSearch('');
     let value = search.trim().replace(/\s+/g, '-').toLocaleLowerCase();
-    navigate(`/search/${value}`)
+    navigate(`/search/${value}`);
+  }
+
+  // Function to close the mobile menu
+  function closeMobileMenu() {
+    const menuCheckbox = document.getElementById('menu-btn');
+    if (menuCheckbox) {
+      menuCheckbox.checked = false;
+    }
   }
 
   return (
     <form className='search' onSubmit={handleSearch}>
-      <input type="search" placeholder='Search tourist sposts' required
-      value={search} onChange={e => setSearch(e.target.value)} />
+      <input
+        type="search"
+        placeholder='Search tourist spots'
+        required
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
-      <button type='submit'>
+      <button
+        type='submit'
+        className='search-button'
+        onClick={closeMobileMenu} // Close the mobile menu when the button is clicked
+      >
         <i className='fa fa-search'></i>
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
